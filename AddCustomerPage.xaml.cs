7394b570835ba02bc0d2118d62fdcad20fedbdc2
Matt Brown <tcisbrowm@tuscolacounty.org>
2023-04-05 14:21:24 -0400

    Major update for add customer screen. Button working with pop up and clears data fields.

--- AddCustomerPage.xaml.cs
+++ AddCustomerPage.xaml.cs
@@ -6,4 +6,26 @@ public partial class AddCustomerPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+
+            Fname.Text = "";
+            Lname.Text = "";
+            Addr.Text = "";
+            Pnum.Text = "";
+            Picker.Date = DateTime.Now;
+            YN.Text = "";
+            notes.Text = "";
+            await DisplayAlert("Customer Added", "You have successfully added a new customer.", "OK");
+
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Error", ex.Message, "OK");
+		}
+		
+    }
 }
